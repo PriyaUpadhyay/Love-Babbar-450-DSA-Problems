2e8280448cb0348196e6551e9439f5ef7dee2b2a
PriyaUpadhyay <68734957+PriyaUpadhyay@users.noreply.github.com>
2021-09-12 11:13:45 +0530

    Update stringReverse.java

--- reverseArray/stringReverse.java
+++ reverseArray/stringReverse.java
@@ -1,1 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t-->0){
+		    String str = sc.nextLine();
+		    System.out.println(reverse(str));
+		}
+	}
+	static String reverse(String str){
+	    int n = str.length();
+	    if(n==0) return "";
+	    
+	    int l = 0,r = n-1;
+	    char[] ch = str.toCharArray();
+	    while(l<r){
+	        char temp = ch[l];
+	        ch[l++] = ch[r];
+	        ch[r--] = temp;
+	        
+	    }
+	    return new String(ch);
+	}
+}
 
